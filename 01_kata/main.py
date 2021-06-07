775931f81e7e99371d627557c83f539cd66ce830
OMAR SUAREZ <osuarez@OMARs-MBP.home>
2021-06-08 01:27:16 +0200

    feat: add code to make the checkout (apply the rules) and compute the total cost of the shopping cart. Add method 'apply' in the BuyMoreThanNItemsRule to apply the rule.

--- 01_kata/main.py
+++ 01_kata/main.py
@@ -12,6 +12,14 @@ class BuyMoreThanNItemsRule:
         self.number_items = number_items
         self.price = price
 
+    def apply(self, shopping_cart):
+        list_of_items = [i.code for i in shopping_cart]
+        if (list_of_items.count(self.item) >= self.number_items):
+            for i in shopping_cart:
+                if (i.code == self.item):
+                    i.price = self.price
+
+
 
 class PricingRules:
     def __init__(self):
@@ -20,14 +28,30 @@ class PricingRules:
     def append_rule(self, rule):
         self.__list_of_rules.append(rule)
 
+    def get_rules(self):
+        return self.__list_of_rules
+
+    ## TODO: add method to delete all rules
+
 
 class Checkout:
     def __init__(self, rules: PricingRules):
         self.__rules = rules
         self.__shopping_cart = []
 
-    def scan(self, item):
+    def scan(self, item: Item):
         self.__shopping_cart.append(item)
 
     def get_shopping_cart(self):
         return [i.code for i in self.__shopping_cart]
+
+    def checkout(self):
+        for rule in self.__rules.get_rules():
+            rule.apply(self.__shopping_cart)
+
+    def total_cost(self):
+        total = 0
+        print(self.__shopping_cart)
+        for i in self.__shopping_cart:
+            total += i.price
+        return total
--- 01_kata/test_main.py
+++ 01_kata/test_main.py
@@ -15,8 +15,42 @@ def test_checkout_scan_and_get_items():
     item1 = Item(code, name, price)
     item2 = Item(code, name, price)
     rule = BuyMoreThanNItemsRule(code, 2, price)
-    pricing_rules = PricingRules().append_rule(rule)
+    pricing_rules = PricingRules()
+    pricing_rules.append_rule(rule)
     checkout = Checkout(pricing_rules)
     for item in [item1, item2]:
         checkout.scan(item)
     assert checkout.get_shopping_cart() == [item1.code, item2.code]
+
+
+def test_pricing_rules():
+    code = "VOUCHER"
+
+    rule = BuyMoreThanNItemsRule(code, 2, 3.00)
+    pricing_rules = PricingRules()
+    pricing_rules.append_rule(rule)
+    assert pricing_rules.get_rules()[0].price == 3.00
+
+
+def test_checkout_method_from_checkout_object():
+    code = "VOUCHER"
+    name = "Gift Card"
+    price = 5.00
+
+    item1 = Item(code, name, price)
+    item2 = Item(code, name, price)
+
+    rule = BuyMoreThanNItemsRule(code, 2, 3.00)
+
+    pricing_rules = PricingRules()
+    pricing_rules.append_rule(rule)
+
+    checkout = Checkout(pricing_rules)
+
+    for item in [item1, item2]:
+        checkout.scan(item)
+    print(checkout.get_shopping_cart())
+    checkout.checkout()
+    print(checkout.get_shopping_cart())
+    result = checkout.total_cost()
+    assert result == 6.00

3a438c107cbbd3e9caf3bf4321fab3fb6f42c415
OMAR SUAREZ <osuarez@OMARs-MBP.home>
2021-06-08 00:15:34 +0200

    feat: add code to implement scan method of the Checkout class (a rule and a pricing rule object need to be created also). Add a test for that.

--- 01_kata/main.py
+++ 01_kata/main.py
@@ -5,3 +5,29 @@ class Item:
         self.code = code
         self.name = name
         self.price = price
+
+class BuyMoreThanNItemsRule:
+    def __init__(self, item: str, number_items: int, price: float):
+        self.item = item
+        self.number_items = number_items
+        self.price = price
+
+
+class PricingRules:
+    def __init__(self):
+        self.__list_of_rules = []
+
+    def append_rule(self, rule):
+        self.__list_of_rules.append(rule)
+
+
+class Checkout:
+    def __init__(self, rules: PricingRules):
+        self.__rules = rules
+        self.__shopping_cart = []
+
+    def scan(self, item):
+        self.__shopping_cart.append(item)
+
+    def get_shopping_cart(self):
+        return [i.code for i in self.__shopping_cart]
--- 01_kata/test_main.py
+++ 01_kata/test_main.py
@@ -1,4 +1,4 @@
-from main import Item
+from main import Item, BuyMoreThanNItemsRule, Checkout, PricingRules
 
 def test_class_item():
     code = "VOUCHER"
@@ -6,3 +6,17 @@ def test_class_item():
     price = 5.00
     item = Item(code, name, price)
     assert (item.code, item.name, item.price) == (code, name, price)
+
+
+def test_checkout_scan_and_get_items():
+    code = "VOUCHER"
+    name = "Gift Card"
+    price = 5.00
+    item1 = Item(code, name, price)
+    item2 = Item(code, name, price)
+    rule = BuyMoreThanNItemsRule(code, 2, price)
+    pricing_rules = PricingRules().append_rule(rule)
+    checkout = Checkout(pricing_rules)
+    for item in [item1, item2]:
+        checkout.scan(item)
+    assert checkout.get_shopping_cart() == [item1.code, item2.code]

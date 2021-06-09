f2588c7feb6879c008956d2a02e9a308aa96d178
osuarez <omsulo@gmail.com>
2021-06-09 23:20:12 +0200

    feat(test): add new test for class BuyMoreThanNItemsRule.

--- 01_kata/test_main.py
+++ 01_kata/test_main.py
@@ -103,9 +103,13 @@ def test_checkout_method_from_checkout_object_3_items_2_same_rules():
     result = checkout.total_cost()
     assert result == 25.00
 
+def test_rule_buy_more_than_n_items():
+    rule = BuyMoreThanNItemsRule("TSHIRT", 2, 19.00)
+    assert (rule.itemCode, rule.number_items, rule.price) == ("TSHIRT", 2, 19.00)
+
 def test_rule_buy_x_pay_y():
     rule = BuyXPayYRule("VOUCHER", 3, 1)
-    assert (rule.item, rule.n_buy, rule.n_pay) == ("VOUCHER", 3, 1)
+    assert (rule.itemCode, rule.n_buy, rule.n_pay) == ("VOUCHER", 3, 1)
 
 def test_checkout_with_rule_buy_x_pay_y():
     code = "VOUCHER"
